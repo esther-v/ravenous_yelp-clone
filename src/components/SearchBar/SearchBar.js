e63b67af9fc9f9467be8fd4b55ef8b374acac6e9
esther-v <esvalas@gmail.com>
2022-04-21 22:52:32 +0200

    constructor searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,6 +8,15 @@ const sortByOptions = {
 }
 
 class Searchbar extends React.Component {
+    constructor(props){
+        super(props) 
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        }
+        
+    }
 
     renderSortByOptions(){
         return Object.keys(sortByOptions).map((sortByOption) => {
@@ -21,7 +30,7 @@ class Searchbar extends React.Component {
             <div className="SearchBar">
             <div className="SearchBar-sort-options">
                 <ul>
-                    {this.renderSortByOptions}
+                    {this.renderSortByOptions()}
                 </ul>
             </div>
             <div className="SearchBar-fields">
