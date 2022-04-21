26da5ca8227d319605e904222147034ee74fe0e5
esther-v <esvalas@gmail.com>
2022-04-21 17:37:00 +0200

    set up business, businesslist & searchbar components

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'most_reviewed'
+}
+
+class Searchbar extends React.Component {
+
+    renderSortByOptions(){
+        return Object.keys(sortByOptions).map((sortByOption) => {
+            let sortByOptionValue = sortByOptions[sortByOption]
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Searchbar;
