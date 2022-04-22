c9b55370717ce0586ea49aaf96e266d91d07fb7f
esther-v <esvalas@gmail.com>
2022-04-22 16:23:22 +0200

    setting search bar state

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -12,8 +12,10 @@ class Searchbar extends React.Component {
             sortBy: 'best_match'
         }
 
+        this.handleSortByChange = this.handleSortByChange.bind(this)
         this.handleTermChange = this.handleTermChange.bind(this)
         this.handleLocationChange = this.handleLocationChange.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
 
         this.sortByOptions = {
             'Best Match': 'best_match',
@@ -43,6 +45,11 @@ class Searchbar extends React.Component {
         this.setState({location: event.target.value})
     }
 
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+        event.preventDefault()
+    }
+
     renderSortByOptions(){
         return Object.keys(this.sortByOptions).map((sortByOption) => {
             let sortByOptionValue = this.sortByOptions[sortByOption]
@@ -58,6 +65,7 @@ class Searchbar extends React.Component {
     
 
     render() {
+        
         return (
             <div className="SearchBar">
             <div className="SearchBar-sort-options">
@@ -66,11 +74,11 @@ class Searchbar extends React.Component {
                 </ul>
             </div>
             <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
+                <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                <input onChange={this.handleLocationChange} placeholder="Where?" />
             </div>
             <div className="SearchBar-submit">
-                <a href="/">Let's Go</a>
+                <button onClick={this.handleSearch}>Let's Go</button>
             </div>
             </div>
         )
